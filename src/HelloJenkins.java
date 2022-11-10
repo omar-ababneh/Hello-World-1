192eac273dce86f853c74f1129ebe5edf9491c33
OMAR ABDEL MAJEED ABABNEH <oaababneh17@cit.just.edu.jo>
2022-11-10 11:15:15 +0300

    try to update pipeline

--- src/HelloJenkins.java
+++ src/HelloJenkins.java
@@ -1,6 +1,6 @@
 public class HelloJenkins {
     public static void main(String[] args) {
         System.out.println("Hello My name is Omar this is the first pipeline in jenkins !! :)" +
-                "First Modification !! :)");
+                "First Modification !! :) hhhhhh i want to try again the pipeline !! ");
     }
 }
