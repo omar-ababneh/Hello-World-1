6ece22e4ba35f930c560b651378b74b9dcba9efb
OMAR ABDEL MAJEED ABABNEH <oaababneh17@cit.just.edu.jo>
2022-11-10 08:59:39 +0300

    Second Commit

--- src/HelloJenkins.java
+++ src/HelloJenkins.java
@@ -1,5 +1,6 @@
 public class HelloJenkins {
     public static void main(String[] args) {
-        System.out.println("Hello My name is Omar this is the first pipeline in jenkins !! :)");
+        System.out.println("Hello My name is Omar this is the first pipeline in jenkins !! :)" +
+                "First Modification !! :)");
     }
 }
