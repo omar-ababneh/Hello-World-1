b22b3175ad3fe648ce5919917e01ade623258103
OMAR ABDEL MAJEED ABABNEH <oaababneh17@cit.just.edu.jo>
2022-11-10 08:50:58 +0300

    Fisrt Commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+E
+src/HelloJenkins.java,5\6\5678068f294c9274aa1545b7abfa5f209a514c61
---
+++ src/HelloJenkins.java
@@ -0,0 +1,5 @@
+public class HelloJenkins {
+    public static void main(String[] args) {
+        System.out.println("Hello My name is Omar this is the first pipeline in jenkins !! :)");
+    }
+}
